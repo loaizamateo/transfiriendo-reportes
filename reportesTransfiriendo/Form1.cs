3ae33de6407d24dd156528ed056220202e9fdcda
mateo <mateoloaiza1227@gmail.com>
2019-09-13 10:58:33 -0500

    Agregar archivos de proyecto.

---
+++ reportesTransfiriendo/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Windows.Forms;
+using CrystalDecisions.Shared; 
+
+namespace reportesTransfiriendo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //using (Form2 frm1 = new Form2(textBoxCufe.Text, textBoxReport.Text))
+            using (Form2 frm1 = new Form2("2e112ee97a742144a53b90041c65761797ed4f41", "100preFactura"))
+            {
+                frm1.ShowDialog();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ reportesTransfiriendo/Form2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Windows.Forms;
+using CrystalDecisions.Shared;
+using System.IO;
+
+namespace reportesTransfiriendo
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection Conn;
+        SqlCommand comandRpt;
+        SqlDataAdapter dbAdapter;
+        ConnectionInfo rptConnInfo;
+        DataSet dbRecord;
+
+        string strSQLCommand;
+        string strSQLConn;
+        string reporte;
+        private string valueCufe;
+        private string valueReport;
+
+
+        public Form2(string cufe, string report)
+        {
+            InitializeComponent();
+            this.valueCufe = cufe;
+            this.valueReport = report;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            strSQLConn = "Data source= MATEOLOAIZA3D59\\SQLEXPRESS; Initial catalog = transfiriendo_base; User id = mateo; Password= mateo123";
+            strSQLCommand = "DocumentoTributario_g_CufeMejorado";
+            crystalReportViewer1.ToolPanelView = ToolPanelViewType.None;
+
+            var files = Directory.GetFiles(@"reportes", this.valueReport + ".rpt", SearchOption.AllDirectories).Where(s => s.EndsWith(this.valueReport + ".rpt"));
+            int b = 0;
+            foreach (string file in files)
+            {
+                b = 1;
+                reporte = file;
+            }
+
+            if(b == 1)
+            {
+                Conn = new SqlConnection(strSQLConn);
+                comandRpt = new SqlCommand(strSQLCommand, Conn);
+                comandRpt.CommandType = CommandType.StoredProcedure;
+                comandRpt.Parameters.Add("@Cufe",SqlDbType.VarChar, 50).Value = this.valueCufe;
+
+                dbAdapter = new SqlDataAdapter(comandRpt);
+                dbRecord = new DataSet();
+
+                dbAdapter.Fill(dbRecord);
+
+                using (ReportDocument report = new ReportDocument())
+                {
+                    report.Load(reporte);
+                    report.SetDataSource(dbRecord);
+                    rptConnInfo = new ConnectionInfo();
+                    rptConnInfo.ServerName = "MATEOLOAIZA3D59\\SQLEXPRESS";
+                    rptConnInfo.DatabaseName = "transfiriendo_base";
+                    rptConnInfo.UserID = "mateo";
+                    rptConnInfo.Password = "mateo123";
+                    rptConnInfo.Type = ConnectionInfoType.SQL;
+                    foreach (Table t in report.Database.Tables)
+                    {
+                        t.LogOnInfo.ConnectionInfo = rptConnInfo;
+                        t.ApplyLogOnInfo(t.LogOnInfo);
+                    }
+
+                    crystalReportViewer1.ReportSource = report;
+                    crystalReportViewer1.Refresh();
+                }    
+            }
+            else
+            {
+                MessageBox.Show("El reporte digitado no existe");
+            }
+        }
+    }
+}
